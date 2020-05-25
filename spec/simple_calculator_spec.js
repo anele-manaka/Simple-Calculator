ed501cdbbe23ac284a63a249de6f2282ce564da8
anele-manaka <anele.s.manaka@gmail.com>
2020-05-25 17:06:02 +0200

    initial commit

---
+++ spec/simple_calculator_spec.js
@@ -0,0 +1,15 @@
+const {add} = require("../src/simple_calculator")
+
+describe("add", function(){
+    it("should add numbers", function(){
+    //expect(add(15,33)).toEqual(48)
+})
+})
+
+const {multiply} = require("../src/simple_calculator")
+
+describe("multiple", function(){
+    it("should multiply numbers", function(){
+        //expect(multiply(15,3,2)).toEqual(90)
+    })
+})
---
+++ src/simple_calculator.js
@@ -0,0 +1,15 @@
+function add() {
+    var sum = 0
+    for (var i = 0; i < arguments.length; i++) {
+        sum += arguments[i]
+}
+return sum
+}
+function multiply() {
+    var multiply = 1
+    for (var i = 0; i < arguments.length; i++)
+        product += arguments[i]
+    }
+    return product
+
+module.exports = {add, multiply}
